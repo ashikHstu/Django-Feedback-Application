bd257d1cb5831d2f491fa2d404d62547d0c4324a
ashikHstu <ashik.cse.hstu@gmail.com>
2026-06-09 01:17:26 +0600

    showing a detail template

--- reviews/models.py
+++ reviews/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
-    reveiw_text = models.TextField()
+    review_text = models.TextField()
     rating = models.IntegerField()
 
